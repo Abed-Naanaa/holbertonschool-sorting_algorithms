11c7df17ca3d822c2d3bbc8655ccd5f2fea6336e
abed-naanaa <abednaanaa12@gmail.com>
2025-02-07 14:12:45 +0000

    d

--- sort.h
+++ sort.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
+#include <stddef.h>
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
